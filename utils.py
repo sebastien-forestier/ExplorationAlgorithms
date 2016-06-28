7c05c21d392d2beb2ad6d977d5c9a243fb134545
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-06-28 16:54:14 +0200

    utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 def compute_explo(data, mins, maxs, gs=100):
     n = len(mins)
@@ -16,3 +16,25 @@ def compute_explo(data, mins, maxs, gs=100):
             grid[tuple(idxs)] = grid[tuple(idxs)] + 1
         grid[grid > 1] = 1
         return np.sum(grid)
+
+def display_movement(fig, ax, environment, time_step=0.04):
+    fig.show()
+    fig.canvas.draw()
+    ax.set_aspect('equal')
+    ax.set_xlim((-1.5, 1.5))
+    ax.set_ylim((-1.5, 1.5))
+    background = fig.canvas.copy_from_bbox(ax.bbox)
+
+    for i in range(50):
+        start = time.time()
+        fig.canvas.restore_region(background)
+        lines = environment.env.plot_update(ax, i)
+        for line in lines:
+            ax.draw_artist(line)
+        fig.canvas.blit(ax.bbox)
+
+        end = time.time()
+        remain = start + time_step - end
+        if remain > 0:
+            time.sleep(remain)
+    time.sleep(2)
